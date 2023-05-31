2f51f055e64281b206d35437676fa84ed6d79bab
frisjon <frisjon0@gmail.com>
2023-05-31 08:45:10 -0400

    xkb. add script

---
+++ xkb/setup_keyboard_layout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d /usr/share/X11/xkb/ ] && [ -f ./customo ] && [ -f evdev.xml ];then
+  sudo cp -i ./customo /usr/share/X11/xkb/symbols
+  sudo cp -i ./evdev.xml /usr/share/X11/xkb/rules.xml
+  # debian
+  sudo dpkg-reconfigure xkb-data
+  setxkbmap customo
+else
+  echo "Failed. Either /usr/share/X11/xkb/ is not a dir or files ./customo ./evdev.xml do not exist"
+fi
+echo "Done"
