4fe11a387e59c01fd6669286a71d167a205fbc19
frisjon <frisjon0@gmail.com>
2023-06-10 23:39:12 -0400

    script. fix autostart script

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -3,5 +3,5 @@ $HOME/.local/bin/xcv mocha
 xsetroot -solid "#fa0"
 dunst &
 
-[ ! -z $(prep -x bspwm) ] && $HOME/.local/bin/bspwm_bar.sh &
-[ ! -z $(prep -x dwm) ] && $HOME/.local/bin/dwm_bar.sh &
+[ ! -z $(pgrep -x bspwm) ] && $HOME/.local/bin/bspwm_bar.sh &
+[ ! -z $(pgrep -x dwm) ] && $HOME/.local/bin/dwm_bar.sh &
