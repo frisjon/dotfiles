eda7e69707dcef4252cfc5c989118264238c5530
frisjon <frisjon0@gmail.com>
2023-10-22 17:23:58 -0300

    script to update emacs

--- .local/bin/wemacs.sh
+++ .local/bin/wemacs.sh
@@ -2,14 +2,22 @@
 
 DOTFILES_DIR=~/dotfiles/.emacs.d
 EMACS_DIR=$(wslpath "$(wslvar USERPROFILE)")/AppData/Roaming/.emacs.d
+SUBDIR_LIST="themes config lisp"
+
+_diff_and_copy_file_from_to () {
+  echo "$2/$1 -> $3/$1"
+  /usr/bin/diff $2/$1 $3/$1 &> /dev/null
+  if [ ! $? -eq 0 ] || [ ! -f $3/$1 ]; then
+    /usr/bin/cp -i $2/$1 $3/$1
+  fi
+}
 
 _diff_and_copy () {
   echo "$1 -> $2"
-  for file in $1/*.el; do
+  for file in $1/*; do
     file_dst=$(echo $file | rev | cut -d'/' -f 1 | rev)
     /usr/bin/diff $file $2/$file_dst &> /dev/null
-    if [ ! $? -eq 0 ] && [ ! -f $1/../$file_dst ]; then
-      #echo $1/../$file_dst
+    if [ ! $? -eq 0 ] || [ ! -f $2/$file_dst ]; then
       /usr/bin/cp -i $file $2/$file_dst
     fi
   done
@@ -18,12 +26,24 @@ _diff_and_copy () {
 case $1 in
   "d2w")
     echo "Dotfiles to Windows"
-    _diff_and_copy $DOTFILES_DIR $EMACS_DIR
-    _diff_and_copy "$DOTFILES_DIR/lisp" "$EMACS_DIR/lisp"
+    _diff_and_copy_file_from_to init.el $DOTFILES_DIR $EMACS_DIR
+    _diff_and_copy_file_from_to custom-vars.el $DOTFILES_DIR $EMACS_DIR
+    for dir in $SUBDIR_LIST;do
+      if [ ! -d $EMACS_DIR/$dir ]; then
+        mkdir -p "$EMACS_DIR/$dir"
+      fi
+      _diff_and_copy "$DOTFILES_DIR/$dir" "$EMACS_DIR/$dir"
+    done
   ;;
   "w2d")
-    _diff_and_copy $EMACS_DIR $DOTFILES_DIR
-    _diff_and_copy "$EMACS_DIR/lisp" "$DOTFILES_DIR/lisp"
+    _diff_and_copy_file_from_to init.el $EMACS_DIR $DOTFILES_DIR
+    _diff_and_copy_file_from_to custom-vars.el $EMACS_DIR $DOTFILES_DIR
+    for dir in $SUBDIR_LIST;do
+      if [ ! -d $DOTFILES_DIR/$dir ]; then
+        mkdir -p "$DOTFILES_DIR/$dir"
+      fi
+      _diff_and_copy "$EMACS_DIR/$dir" "$DOTFILES_DIR/$dir"
+    done
   ;;
   *)
     echo no option
