8293e44cc2c84f3ad287f9516cac22e469abf140
frisjon <frisjon0@gmail.com>
2024-01-25 22:51:30 -0300

    emacs. add mouse actions to modeline

--- .emacs.d/config/modeline.el
+++ .emacs.d/config/modeline.el
@@ -4,51 +4,51 @@
   (defun fris-modeline/reeval-faces-on-theme-change ()
     (interactive)
     (set-face-attribute 'fris-modeline--major-mode-face nil
-                        :background (ef-themes-with-colors bg-red-intense)
-                        :foreground (ef-themes-with-colors fg-mode-line))
+      :background (ef-themes-with-colors bg-red-intense)
+      :foreground (ef-themes-with-colors fg-mode-line))
 
     (set-face-attribute 'fris-modeline--major-mode-face-inactive nil
-                        :background (ef-themes-with-colors bg-tab-bar)
-                        :foreground (ef-themes-with-colors fg-mode-line))
+      :background (ef-themes-with-colors bg-tab-bar)
+      :foreground (ef-themes-with-colors fg-mode-line))
 
     (set-face-attribute 'fris-modeline--buffer-name-face nil
-                        :background (ef-themes-with-colors bg-green-intense)
-                        :foreground (ef-themes-with-colors fg-mode-line))
+      :background (ef-themes-with-colors bg-green-intense)
+      :foreground (ef-themes-with-colors fg-mode-line))
 
     (set-face-attribute 'fris-modeline--buffer-name-face-inactive nil
-                        :background (ef-themes-with-colors bg-tab-bar)
-                        :foreground (ef-themes-with-colors fg-mode-line))
+      :background (ef-themes-with-colors bg-tab-bar)
+      :foreground (ef-themes-with-colors fg-mode-line))
 
     (set-face-attribute 'fris-modeline--mule-face nil
-                        :background (ef-themes-with-colors bg-blue-intense)
-                        :foreground (ef-themes-with-colors fg-mode-line))))
+      :background (ef-themes-with-colors bg-blue-intense)
+      :foreground (ef-themes-with-colors fg-mode-line))))
 
 (when 'ef-themes-with-colors
   (progn
     ;; Faces ------------------------------------------------------------------
     (defface fris-modeline--major-mode-face
       `((t :background ,(ef-themes-with-colors bg-red-intense)
-           :foreground ,(ef-themes-with-colors fg-mode-line)))
+          :foreground ,(ef-themes-with-colors fg-mode-line)))
       "Face for custom modeline. Used for major mode in active modeline")
 
     (defface fris-modeline--major-mode-face-inactive
       `((t :background ,(ef-themes-with-colors bg-tab-bar)
-           :foreground ,(ef-themes-with-colors fg-mode-line)))
+          :foreground ,(ef-themes-with-colors fg-mode-line)))
       "Face for custom modeline. Used for major mode in inactive modeline")
 
     (defface fris-modeline--buffer-name-face
       `((t :background ,(ef-themes-with-colors bg-green-intense)
-           :foreground ,(ef-themes-with-colors fg-mode-line)))
+          :foreground ,(ef-themes-with-colors fg-mode-line)))
       "Face for custom modeline. Used for buffer name in active modeline")
 
     (defface fris-modeline--buffer-name-face-inactive
       `((t :background ,(ef-themes-with-colors bg-tab-bar)
-           :foreground ,(ef-themes-with-colors fg-mode-line)))
+          :foreground ,(ef-themes-with-colors fg-mode-line)))
       "Face for custom modeline. Used for buffer name in inactive modeline")
 
     (defface fris-modeline--mule-face
       `((t :background ,(ef-themes-with-colors bg-blue-intense)
-           :foreground ,(ef-themes-with-colors fg-mode-line)))
+          :foreground ,(ef-themes-with-colors fg-mode-line)))
       "Face for custom modeline. Used for mule info")
 
     ;; Functions --------------------------------------------------------------
@@ -63,60 +63,73 @@
     (defun fris-modeline/eol-style-string (eol)
       "Return string showing eol style. To be used in custom modeline"
       (cond ((= eol 0) "LF")
-            ((= eol 1) "CRLF")
-            ((= eol 2) "CR")
-            (t "")))
+        ((= eol 1) "CRLF")
+        ((= eol 2) "CR")
+        (t "")))
 
     (defun fris-modeline/mule-string ()
       "Return string to display in modeline. Shows buffer encoding and eol style"
       (let ((encoding (symbol-name buffer-file-coding-system))
-            (eol (coding-system-eol-type buffer-file-coding-system)))
+             (eol (coding-system-eol-type buffer-file-coding-system)))
         (format " %s %s "
-                (fris-modeline/buffer-coding-system-string encoding)
-                (fris-modeline/eol-style-string eol))))
+          (fris-modeline/buffer-coding-system-string encoding)
+          (fris-modeline/eol-style-string eol))))
 
     ;; Variables --------------------------------------------------------------
     (defvar-local fris-modeline--major-mode
-        '(:eval (if (mode-line-window-selected-p)
-                    (propertize (fris-modeline/major-mode-string) 'face
-                                'fris-modeline--major-mode-face)
-                  (propertize (fris-modeline/major-mode-string) 'face
-                              'fris-modeline--major-mode-face-inactive)))
+      '(:eval (if (mode-line-window-selected-p)
+                (propertize (fris-modeline/major-mode-string)
+                  'face 'fris-modeline--major-mode-face
+                  'local-map '(keymap (mode-line keymap
+                                        (mouse-3  menu-item "menu bar")
+                                        (mouse-1 .  describe-mode)))
+                  )
+                (propertize (fris-modeline/major-mode-string) 'face
+                  'fris-modeline--major-mode-face-inactive)))
       "Local variable to show major mode. To be used in custom modeline")
 
     (defvar-local fris-modeline--buffer-name
-        '(:eval
-          (if (mode-line-window-selected-p)
-              (propertize
-               (fris-modeline/buffer-name-string)
-               'face 'fris-modeline--buffer-name-face)
-            (propertize
+      '(:eval
+         (if (mode-line-window-selected-p)
+           (propertize
+             (fris-modeline/buffer-name-string)
+             'face 'fris-modeline--buffer-name-face
+             'help-echo " Buffer name"
+             'local-map '(keymap (mode-line keymap
+                                   (mouse-3 . previous-buffer)
+                                   (mouse-1 . next-buffer))))
+           (propertize
              (fris-modeline/buffer-name-string)
              'face 'fris-modeline--buffer-name-face-inactive)))
       "Local variable to show buffer name. To be used in custom modeline")
 
     (defvar-local fris-modeline--time
-        '(:eval
-          (when (mode-line-window-selected-p) global-mode-string))
+      '(:eval
+         (when (mode-line-window-selected-p) global-mode-string))
       "Local variable to show time. To be used in custom modeline")
 
     (defun fris-modeline/buffer-coding-system-string (encoding)
       "Return string showing buffer encoding. To be used in custom modeline"
       (cond ((string= encoding "utf-8-unix") "UTF-8")
-            (t encoding)))
+        (t encoding)))
 
     (defvar-local fris-modeline--mule
-        '(:eval
-          (when (mode-line-window-selected-p)
-            (propertize (fris-modeline/mule-string)
-                        'face 'fris-modeline--mule-face)))
+      '(:eval
+         (when (mode-line-window-selected-p)
+           (propertize (fris-modeline/mule-string)
+             'face 'fris-modeline--mule-face
+             'local-map '(keymap (mode-line keymap
+                                   (mouse-1 . (lambda ()
+                                                (interactive)
+                                                (mode-line-change-eol
+                                                  (make-sparse-keymap)))))))))
       "Local variable to mule info. To be used in custom modeline")
 
     ;; Add custom variables ---------------------------------------------------
     (dolist (var '(fris-modeline--buffer-name
-                   fris-modeline--major-mode
-                   fris-modeline--time
-                   fris-modeline--mule))
+                    fris-modeline--major-mode
+                    fris-modeline--time
+                    fris-modeline--mule))
       (put var 'risky-local-variable t))
 
     ;; set modeline
@@ -134,21 +147,21 @@
 
     ;; Modeline ---------------------------------------------------------------
     (setq-default
-     mode-line-format
-     '(:eval
-       (fris/simple-mode-line-render
-        ;; left
-        (format-mode-line
-         '("%e" mode-line-modified mode-line-remote " "
-           fris-modeline--buffer-name fris-modeline--major-mode
-           " (%l,%C) %I "))
-        ;; right
-        (format-mode-line
-         '("" fris-modeline--mule (vc-mode vc-mode)
-           " " fris-modeline--time)))))
+      mode-line-format
+      '(:eval
+         (fris/simple-mode-line-render
+           ;; left
+           (format-mode-line
+             '("%e" mode-line-modified mode-line-remote " "
+                fris-modeline--buffer-name fris-modeline--major-mode
+                " (%l,%C) %I "))
+           ;; right
+           (format-mode-line
+             '("" fris-modeline--mule (vc-mode vc-mode)
+                " " fris-modeline--time)))))
 
     ;; Hook to update faces in modeline ---------------------------------------
     (add-hook 'ef-themes-post-load-hook
-              'fris-modeline/reeval-faces-on-theme-change)
+      'fris-modeline/reeval-faces-on-theme-change)
     ;; warning. the hook is used by ef-themes-select. dont use load-theme
-))
+    ))
--- todo.md
+++ todo.md
@@ -6,6 +6,6 @@
 ## emacs
 ~~1. display date and time in modeline on the right side~~
 ~~2. re-evaluate custom modeline faces when theme changes~~
-3. add mouse actions to modeline
+~~3. add mouse actions to modeline~~
 4. edit modeline for "read only buffer" and "edited buffer"
 5. use utf-8 characters in emacs and modeline (on windows)
