47bf0499a2c31a82f4f01217248eaab63488afb9
frisjon <frisjon0@gmail.com>
2024-09-19 18:51:11 -0300

    update emacs

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -7,13 +7,13 @@
 
 ;; isearch
 (setq lazy-highlight-cleanup nil
-  isearch-wrap-pause 'no)
+      isearch-wrap-pause 'no)
 
 ;; ls-lisp
 (setq-default
-  ls-lisp-dirs-first t
-  ls-lisp-use-insert-directory-program nil)
-                                        ;:hook (dired-mode . dired-hide-details-mode)
+ ls-lisp-dirs-first t
+ ls-lisp-use-insert-directory-program nil)
+;; hook (dired-mode . dired-hide-details-mode)
 
 ;; rotate
 (setq rotate-functions '(rotate:main-vertical))
@@ -24,18 +24,19 @@
 ;;http://xahlee.info/emacs/emacs/emacs_dired_tips.html
 ;; dired
 (setq dired-kill-when-opening-new-dired-buffer t
-  dired-clean-confirm-killing-deleted-buffers nil
-  dired-confirm-shell-command nil
-  dired-no-confirm t
-  dired-recursive-deletes 'always
-  dired-deletion-confirmer '(lambda (x) t))
+      dired-clean-confirm-killing-deleted-buffers nil
+      dired-confirm-shell-command nil
+      dired-no-confirm t
+      dired-recursive-deletes 'always
+      dired-deletion-confirmer '(lambda (x) t))
 
 ;; eglot
-(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-(add-to-list 'eglot-server-programs '((rust-mode) . ("rust-analyzer​")))
-(add-to-list 'eglot-server-programs
-  '((rust-ts-mode rust-mode) .
-     ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
+(with-eval-after-load "eglot"
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+  (add-to-list 'eglot-server-programs '((rust-mode) . ("rust-analyzer​")))
+  (add-to-list 'eglot-server-programs
+               '((rust-ts-mode rust-mode) .
+                 ("rust-analyzer" :initializationOptions (:check (:command "clippy"))))))
 ;;  :hook
 ;;  (c-mode . eglot-ensure)
 ;;  (c++-mode . eglot-ensure)
@@ -52,9 +53,9 @@
 
 ;; elec-pair
 (setq electric-pair-pairs
-  '((?\" . ?\")
-     (?\[ . ?\])
-     (?\{ . ?\})))
+      '((?\" . ?\")
+        (?\[ . ?\])
+        (?\{ . ?\})))
 (electric-pair-mode t)
 
 ;; paren
@@ -70,33 +71,35 @@
 ;; org
 (setq-default org-support-shift-select t)
 (setq org-publish-project-alist
-  '(("orgblog_test"
-      :base-directory "/plinkx:local_wsl:~/orgblog_test/"
-      :publishing-function org-html-publish-to-html
-      :publishing-directory "/plinkx:local_wsl:~/orgblog_test/public/"
-      :section-numbers t
-      :recursive t
-      :with-toc t
-      :html-preamble t
-      :exclude "private.org"
-      :html-head "<link rel=\"stylesheet\" href=\"../../res/style.css\" type=\"text/css\"/>"))
-  org-footnote-section nil
-  org-html-postamble-format
-  '(("en" "<p class=\"author\">Author: %a (%e)</p>
+      '(("orgblog_test"
+         :base-directory "/plinkx:local_wsl:~/orgblog_test/"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "/plinkx:local_wsl:~/orgblog_test/public/"
+         :section-numbers t
+         :recursive t
+         :with-toc t
+         :html-preamble t
+         :exclude "private.org"
+         :html-head "<link rel=\"stylesheet\" href=\"../../res/style.css\" type=\"text/css\"/>"))
+      org-footnote-section nil
+      org-html-postamble-format
+      '(("en" "<p class=\"author\">Author: %a (%e)</p>
 <p class=\"date\">Date: %d</p>
 <p class=\"creator\">%c</p>")))
-(org-element-cache-reset)
+
+(with-eval-after-load "org"
+  (org-element-cache-reset))
 
 ;; tab-bar
 (setq-default tab-bar-close-button-show nil
-  tab-bar-tab-hints t)
+              tab-bar-tab-hints t)
 
 ;; window
 (setq-default pop-up-windows nil)
 (advice-add 'other-window :before
-  (defun other-window-split-if-single (&rest _)
-    "Split the frame if there is a single window."
-    (when (one-window-p) (split-window-sensibly))))
+            (defun other-window-split-if-single (&rest _)
+              "Split the frame if there is a single window."
+              (when (one-window-p) (split-window-sensibly))))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -121,13 +124,13 @@
 
 ;; savehist
 (setq savehist-file "~/.emacs.d/savehist"
-  history-length t
-  history-delete-duplicates t
-  savehist-save-minibuffer-history 1
-  savehist-additional-variables
-  '(kill-ring
-     search-ring
-     regexp-search-ring))
+      history-length t
+      history-delete-duplicates t
+      savehist-save-minibuffer-history 1
+      savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
 (savehist-mode t)
 
 ;; autorevert
@@ -147,7 +150,97 @@
               display-time-default-load-average nil
               display-time-load-average "")
 
+
+;; ido
+(ido-mode t)
+
+;; hideshow
+(setq
+ hs-hide-comments nil
+ hs-isearch-open 'x)
+(defvar hs-special-modes-alist
+  (mapcar 'purecopy
+          '((c-mode "{" "}" "/[*/]" nil nil)
+            (rust-mode "{" "}" "/[*/]" nil nil)
+            (c++-mode "{" "}" "/[*/]" nil nil)
+            ;;(emacs-lisp-mode "(" ")" nil nil nil nil)
+            ;;(bibtex-mode ("@\\S(*\\(\\s(\\)" 1))
+            (java-mode "{" "}" "/[*/]" nil nil)
+            (js-mode "{" "}" "/[*/]" nil))))
+;;(hs-minor-mode)
+(dolist
+    (prog-modes '(c-mode-common-hook
+                  emacs-lisp-mode-hook
+                  rust-mode-hook
+                  c++-mode-hook
+                  js-mode-hook
+                  java-mode-hook))
+  (add-hook prog-modes 'hs-minor-mode))
+
+;; ibuffer
+;; https://www.emacswiki.org/emacs/IbufferMode#h5o-1
+;; Use human readable Size column instead of original one
+(with-eval-after-load "ibuffer"
+
+  (define-ibuffer-column size-h
+    (:name "Size"
+           :inline t
+           :summarizer
+           (lambda (column-strings)
+             (let ((total 0))
+               (dolist (string column-strings)
+                 (setq total
+                       ;; like, ewww ...
+                       (+ (float (ajv/human-readable-file-sizes-to-bytes string))
+                          total)))
+               (ajv/bytes-to-human-readable-file-sizes total)))    ;; :summarizer nil
+           )
+    (ajv/bytes-to-human-readable-file-sizes (buffer-size)))
+  ;; Modify the default ibuffer-formats
+  (setq ibuffer-formats
+        '((mark modified read-only locked " "
+                (name 20 20 :left :elide) " "
+                ;;(size-h 11 -1 :right) " "
+                (mode 16 16 :left :elide) " "
+                filename-and-process)
+          (mark " "
+                (name 16 -1) " " filename))
+        ibuffer-default-sorting-mode 'major-mode))
+
+(load-file "~/.emacs.d/lisp/hideshowvis.el")
+(require 'hideshowvis)
+(hideshowvis-minor-mode)
+
+(load-file "~/.emacs.d/lisp/selection-highlight-mode.el")
+(selection-highlight-mode)
+
+(load-file "~/.emacs.d/lisp/ibuffer-vc.el")
+(setq ibuffer-formats
+  '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                vc-relative-file))
+        ibuffer-vc-skip-if-remote nil)
+
+;;(load-file "~/.emacs.d/lisp/zen-mode.el")
+;;(require 'column-marker)
+
+;; themes
+(push "~/.emacs.d/themes" load-path)
+;;(load-file "~/.emacs.d/themes/one-themes.el")
+;;(load-theme 'one-light)
+
+;;(load-file "~/.emacs.d/themes/tsoding-theme.el")
+;;(load-theme 'tsoding)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'use-package)
 
 (use-package edwina
   :ensure t
@@ -215,111 +308,3 @@
 (use-package rust-mode
   :ensure t
   :defer 2)
-
-(use-package ido
-  :ensure nil
-  :config
-  (ido-mode t))
-
-(use-package hideshow
-  :ensure nil
-  :defer 1
-  :config
-  (setq
-    hs-hide-comments nil
-    hs-isearch-open 'x)
-  (defvar hs-special-modes-alist
-    (mapcar 'purecopy
-      '((c-mode "{" "}" "/[*/]" nil nil)
-         (rust-mode "{" "}" "/[*/]" nil nil)
-         (c++-mode "{" "}" "/[*/]" nil nil)
-         ;;(emacs-lisp-mode "(" ")" nil nil nil nil)
-         ;;(bibtex-mode ("@\\S(*\\(\\s(\\)" 1))
-         (java-mode "{" "}" "/[*/]" nil nil)
-         (js-mode "{" "}" "/[*/]" nil))))
-  ;;(hs-minor-mode)
-  (dolist
-    (prog-modes '(c-mode-common-hook
-                   emacs-lisp-mode-hook
-                   rust-mode-hook
-                   c++-mode-hook
-                   js-mode-hook
-                   java-mode-hook))
-    (add-hook prog-modes 'hs-minor-mode))
-  )
-
-(use-package ibuffer
-  :ensure nil
-  :config
-  ;; https://www.emacswiki.org/emacs/IbufferMode#h5o-1
-  ;; Use human readable Size column instead of original one
-  (define-ibuffer-column size-h
-    (:name "Size"
-           :inline t
-           :summarizer
-           (lambda (column-strings)
-             (let ((total 0))
-               (dolist (string column-strings)
-                 (setq total
-                       ;; like, ewww ...
-                       (+ (float (ajv/human-readable-file-sizes-to-bytes string))
-                          total)))
-               (ajv/bytes-to-human-readable-file-sizes total)))    ;; :summarizer nil
-           )
-    (ajv/bytes-to-human-readable-file-sizes (buffer-size)))
-
-  ;; Modify the default ibuffer-formats
-  (setq ibuffer-formats
-        '((mark modified read-only locked " "
-                (name 20 20 :left :elide) " "
-                ;;(size-h 11 -1 :right) " "
-                (mode 16 16 :left :elide) " "
-                filename-and-process)
-          (mark " "
-                (name 16 -1) " " filename))
-        ibuffer-default-sorting-mode 'major-mode))
-
-;;(load-file "~/.emacs.d/lisp/hideshowvis.el")
-(use-package hideshowvis
-  :ensure nil
-  :defer 3
-  :config (hideshowvis-minor-mode))
-
-;;(load-file "~/.emacs.d/lisp/selection-highlight-mode.el")
-(use-package selection-highlight-mode
-  :ensure nil
-  :defer 2
-  :config
-  (selection-highlight-mode))
-
-;;(load-file "~/.emacs.d/lisp/ibuffer-vc.el")
-(use-package ibuffer-vc
-  :ensure nil
-  :config
-  (setq ibuffer-formats
-        '((mark modified read-only vc-status-mini " "
-                (name 18 18 :left :elide)
-                " "
-                (size 9 -1 :right)
-                " "
-                (mode 16 16 :left :elide)
-                " "
-                (vc-status 16 16 :left)
-                " "
-                vc-relative-file))
-        ibuffer-vc-skip-if-remote nil))
-
-;;(load-file "~/.emacs.d/lisp/zen-mode.el")
-;;(use-package zen-mode
-;;  :ensure nil
-;;  :defer 3)
-
-;;(require 'column-marker)
-
-;; themes
-(push "~/.emacs.d/themes" load-path)
-;;(load-file "~/.emacs.d/themes/one-themes.el")
-;;(load-theme 'one-light)
-
-;(load-file "~/.emacs.d/themes/tsoding-theme.el")
-;(load-theme 'tsoding)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 ;;(setq use-package-compute-statistics t)
+(setq gc-cons-threshold (* 2000 1000 1000))
 (make-directory "~/.emacs.d/config/" t)
 (push "~/.emacs.d/config" load-path)
 
@@ -17,10 +18,10 @@
 ;;(unless (package-installed-p 'use-package)
 ;;   (package-install 'use-package))
 
-(require 'use-package)
 (load-file "~/.emacs.d/config/settings.el")
 (load-file "~/.emacs.d/config/functions_binding.el")
 (load-file "~/.emacs.d/config/packages.el")
 (load-file "~/.emacs.d/config/modeline2.el")
 (fris/remove-box-attr-from-modeline)
 ;;(load-file "~/.emacs.d/feeds.el")
+(setq gc-cons-threshold 800000)
