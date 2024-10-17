c1ddd6e3d2247305731bcd0ba5b81c3de98def66
frisjon <frisjon0@gmail.com>
2024-10-17 15:46:01 -0300

    update emacs

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -1,4 +1,6 @@
 ;; -*- lexical-binding: t; -*-
+(require 'use-package)
+
 ;; tramp
 (use-package tramp
   :defer 5
@@ -8,115 +10,132 @@
   (add-to-list 'tramp-remote-path "/home/mk/.cargo/bin"))
 
 ;; isearch
-(setq lazy-highlight-cleanup nil
-      isearch-wrap-pause 'no)
+(use-package isearch
+  :config
+  (setq lazy-highlight-cleanup nil
+        isearch-wrap-pause 'no))
 
 ;; ls-lisp
-(setq-default
- ls-lisp-dirs-first t
- ls-lisp-use-insert-directory-program nil)
-;; hook (dired-mode . dired-hide-details-mode)
+(use-package ls-lisp
+  :config
+  (setq-default ls-lisp-dirs-first t
+                ls-lisp-use-insert-directory-program nil)
+  :hook
+  (dired-mode . dired-hide-details-mode))
 
-;; rotate
-(setq rotate-functions '(rotate:main-vertical))
+(use-package rotate
+  :config
+  (setq rotate-functions '(rotate:main-vertical)))
 
-;; windmove
-(windmove-mode t)
+(use-package windmove
+  :config
+  (windmove-mode t))
 
 ;;http://xahlee.info/emacs/emacs/emacs_dired_tips.html
-;; dired
-(setq dired-kill-when-opening-new-dired-buffer t
-      dired-clean-confirm-killing-deleted-buffers nil
-      dired-confirm-shell-command nil
-      dired-no-confirm t
-      dired-recursive-deletes 'always
-      dired-deletion-confirmer '(lambda (x) t))
+(use-package dired
+  :config
+  (setq dired-kill-when-opening-new-dired-buffer t
+        dired-clean-confirm-killing-deleted-buffers nil
+        dired-confirm-shell-command nil
+        dired-no-confirm t
+        dired-recursive-deletes 'always
+        dired-deletion-confirmer '(lambda (x) t)))
 
-;; eglot
-(with-eval-after-load "eglot"
-  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-  (add-to-list 'eglot-server-programs '((rust-mode) . ("rust-analyzer​")))
-  (add-to-list 'eglot-server-programs
-               '((rust-ts-mode rust-mode) .
-                 ("rust-analyzer" :initializationOptions (:check (:command "clippy"))))))
-;;  :hook
-;;  (c-mode . eglot-ensure)
-;;  (c++-mode . eglot-ensure)
-;;  (rust-mode . eglot-ensure)
+(use-package eglot
+  :config
+  (with-eval-after-load "eglot"
+    (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+    (add-to-list 'eglot-server-programs '((rust-mode) . ("rust-analyzer​")))
+    (add-to-list 'eglot-server-programs
+                 '((rust-ts-mode rust-mode) .
+                   ("rust-analyzer" :initializationOptions (:check (:command "clippy"))))))
+  :hook
+  (c-mode . eglot-ensure)
+  (c++-mode . eglot-ensure)
+  (rust-mode . eglot-ensure))
 
-;; uniquify
-(setq uniquify-buffer-name-style 'forward)
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'forward))
 
-;; electric
-(electric-indent-mode t)
+(use-package electric
+  :config
+  (electric-indent-mode t))
 
-;; delsel
-(delete-selection-mode t)
+(use-package delsel
+  :config
+  (delete-selection-mode t))
 
-;; elec-pair
-(setq electric-pair-pairs
-      '((?\" . ?\")
-        (?\[ . ?\])
-        (?\{ . ?\})))
-(electric-pair-mode t)
+(use-package elec-pair
+  :config
+  (setq electric-pair-pairs
+        '((?\" . ?\")
+          (?\[ . ?\])
+          (?\{ . ?\})))
+  (electric-pair-mode t))
 
-;; paren
-(show-paren-mode t)
+(use-package paren
+  :config
+  (show-paren-mode t))
 
-;; simple
-(setq-default indent-tabs-mode nil)
-(column-number-mode)
+(use-package simple
+  :config
+  (setq-default indent-tabs-mode nil)
+  (column-number-mode))
 
-;; cc-vars
-(setq-default c-basic-offset 4)
+(use-package cc-vars
+  :config
+  (setq-default c-basic-offset 4))
 
-;; org
-(setq-default org-support-shift-select t)
-(setq org-publish-project-alist
-      '(("orgblog_test"
-         :base-directory "/plinkx:local_wsl:~/orgblog_test/"
-         :publishing-function org-html-publish-to-html
-         :publishing-directory "/plinkx:local_wsl:~/orgblog_test/public/"
-         :section-numbers t
-         :recursive t
-         :with-toc t
-         :html-preamble t
-         :exclude "private.org"
-         :html-head "<link rel=\"stylesheet\" href=\"../../res/style.css\" type=\"text/css\"/>"))
-      org-footnote-section nil
-      org-html-postamble-format
-      '(("en" "<p class=\"author\">Author: %a (%e)</p>
+(use-package org
+  :config
+  (setq-default org-support-shift-select t)
+  (setq org-publish-project-alist
+        '(("orgblog_test"
+           :base-directory "/plinkx:local_wsl:~/orgblog_test/"
+           :publishing-function org-html-publish-to-html
+           :publishing-directory "/plinkx:local_wsl:~/orgblog_test/public/"
+           :section-numbers t
+           :recursive t
+           :with-toc t
+           :html-preamble t
+           :exclude "private.org"
+           :html-head "<link rel=\"stylesheet\" href=\"../../res/style.css\" type=\"text/css\"/>"))
+        org-footnote-section nil
+        org-html-postamble-format
+        '(("en" "<p class=\"author\">Author: %a (%e)</p>
 <p class=\"date\">Date: %d</p>
 <p class=\"creator\">%c</p>")))
-
-(with-eval-after-load "org"
   (org-element-cache-reset))
 
-;; tab-bar
-(setq-default tab-bar-close-button-show nil
-              tab-bar-tab-hints t)
+(use-package tab-bar
+  :config
+  (setq-default tab-bar-close-button-show nil
+                tab-bar-tab-hints t))
 
-;; window
-(setq-default pop-up-windows nil)
-(advice-add 'other-window :before
-            (defun other-window-split-if-single (&rest _)
-              "Split the frame if there is a single window."
-              (when (one-window-p) (split-window-sensibly))))
+(use-package window
+  :config
+  (setq-default pop-up-windows nil)
+  (advice-add 'other-window :before
+              (defun other-window-split-if-single (&rest _)
+                "Split the frame if there is a single window."
+                (when (one-window-p) (split-window-sensibly)))))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
 
-;; display-line-numbers
-(global-display-line-numbers-mode t)
-;;(dolist (mode '(org-mode-hook
-;;                 dired-mode-hook
-;;                 term-mode-hook
-;;                 buffer-list-update-hook
-;;                 help-mode-hook
-;;                 eshell-mode-hook))
-;;  (add-hook mode #'(lambda () (display-line-numbers-mode -1))))
+(use-package display-line-numbers
+  :config
+  (global-display-line-numbers-mode t)
+  (dolist (mode '(org-mode-hook
+                  dired-mode-hook
+                  term-mode-hook
+                  buffer-list-update-hook
+                  help-mode-hook
+                  eshell-mode-hook))
+    (add-hook mode #'(lambda () (display-line-numbers-mode -1)))))
 
 ;; for long line files
 (global-so-long-mode 1)
@@ -127,66 +146,74 @@
 ;; saveplace
 ;;(save-place-mode nil)
 
-;; savehist
-(setq savehist-file "~/.emacs.d/savehist"
-      history-length t
-      history-delete-duplicates t
-      savehist-save-minibuffer-history 1
-      savehist-additional-variables
-      '(kill-ring
-        search-ring
-        regexp-search-ring))
-(savehist-mode t)
+(use-package savehist
+  :config
+  (setq savehist-file "~/.emacs.d/savehist"
+        history-length t
+        history-delete-duplicates t
+        savehist-save-minibuffer-history 1
+        savehist-additional-variables
+        '(kill-ring
+          search-ring
+          regexp-search-ring))
+  (savehist-mode t))
 
-;; autorevert
-(setq global-auto-revert-non-file-buffers t)
-(global-auto-revert-mode t)
+(use-package autorevert
+  :config
+  (setq global-auto-revert-non-file-buffers t)
+  (global-auto-revert-mode t))
 
 ;; Dont warn for following symlinked files
-;; vc-hooks
-(setq vc-follow-symlinks t)
+(use-package vc-hooks
+  :config
+  (setq vc-follow-symlinks t))
 
-;; fringe
-(fringe-mode '(10 . 0))
+(use-package fringe
+  :config
+  (fringe-mode '(10 . 0)))
 
-;; time
-(display-time-mode t)
-(setq-default display-time-format "%a %d %b %H:%M"
-              display-time-default-load-average nil
-              display-time-load-average "")
+(use-package time
+  :config
+  (display-time-mode t)
+  (setq-default display-time-format "%a %d %b %H:%M"
+                display-time-default-load-average nil
+                display-time-load-average ""))
 
-;; ido
-(ido-mode t)
-(ido-everywhere t)
+(use-package ido
+  :config
+  (ido-mode t)
+  (ido-everywhere t))
 
-;; hideshow
-(setq
- hs-hide-comments nil
- hs-isearch-open 'x)
-(defvar hs-special-modes-alist
-  (mapcar 'purecopy
-          '((c-mode "{" "}" "/[*/]" nil nil)
-            (rust-mode "{" "}" "/[*/]" nil nil)
-            (c++-mode "{" "}" "/[*/]" nil nil)
-            ;;(emacs-lisp-mode "(" ")" nil nil nil nil)
-            ;;(bibtex-mode ("@\\S(*\\(\\s(\\)" 1))
-            (java-mode "{" "}" "/[*/]" nil nil)
-            (js-mode "{" "}" "/[*/]" nil))))
-;;(hs-minor-mode)
-(dolist
-    (prog-modes '(c-mode-common-hook
-                  emacs-lisp-mode-hook
-                  rust-mode-hook
-                  c++-mode-hook
-                  js-mode-hook
-                  java-mode-hook))
-  (add-hook prog-modes 'hs-minor-mode))
+(use-package hideshow
+  :config
+  (setq
+   hs-hide-comments nil
+   hs-isearch-open 'x)
+  (defvar hs-special-modes-alist
+    (mapcar 'purecopy
+            '((c-mode "{" "}" "/[*/]" nil nil)
+              (rust-mode "{" "}" "/[*/]" nil nil)
+              (c++-mode "{" "}" "/[*/]" nil nil)
+              ;;(emacs-lisp-mode "(" ")" nil nil nil nil)
+              ;;(bibtex-mode ("@\\S(*\\(\\s(\\)" 1))
+              (java-mode "{" "}" "/[*/]" nil nil)
+              (js-mode "{" "}" "/[*/]" nil))))
+  ;;(hs-minor-mode)
+  (dolist
+      (prog-modes '(c-mode-common-hook
+                    emacs-lisp-mode-hook
+                    rust-mode-hook
+                    c++-mode-hook
+                    js-mode-hook
+                    java-mode-hook))
+    (add-hook prog-modes 'hs-minor-mode))
+  )
 
-;; ibuffer
-;; https://www.emacswiki.org/emacs/IbufferMode#h5o-1
-;; Use human readable Size column instead of original one
-(with-eval-after-load "ibuffer"
 
+(use-package ibuffer
+  ;; https://www.emacswiki.org/emacs/IbufferMode#h5o-1
+  ;; Use human readable Size column instead of original one
+  :config
   (define-ibuffer-column size-h
     (:name "Size"
            :inline t
@@ -221,17 +248,17 @@
 
 (load-file "~/.emacs.d/lisp/ibuffer-vc.el")
 (setq ibuffer-formats
-  '((mark modified read-only vc-status-mini " "
-                (name 18 18 :left :elide)
-                " "
-                (size 9 -1 :right)
-                " "
-                (mode 16 16 :left :elide)
-                " "
-                (vc-status 16 16 :left)
-                " "
-                vc-relative-file))
-        ibuffer-vc-skip-if-remote nil)
+      '((mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              (vc-status 16 16 :left)
+              " "
+              vc-relative-file))
+      ibuffer-vc-skip-if-remote nil)
 
 ;;(load-file "~/.emacs.d/lisp/zen-mode.el")
 ;;(require 'column-marker)
@@ -245,7 +272,6 @@
 ;;(load-theme 'tsoding)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'use-package)
 
 (use-package edwina
   :ensure t
