aa267c26b244a9fd4f35ab95ce9c832afad9bb68
frisjon <frisjon0@gmail.com>
2025-08-01 14:48:33 -0300

    add keyb layout and brightnessctl to autostart

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 $HOME/.local/bin/xcv mocha
 xsetroot -solid "#111222"
+setxkbmap customo
+brightnessctl 30%
 dunst &
-
 [ ! -z $(pgrep -x bspwm) ] && $HOME/.local/bin/bspwm_bar.sh &
 [ ! -z $(pgrep -x dwm) ] && $HOME/.local/bin/dwm_bar.sh &
