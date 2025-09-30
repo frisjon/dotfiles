2d934f98007f4f99b409b0dc9ce81a44d54285e1
frisjon <frisjon0@gmail.com>
2025-09-30 18:35:20 -0300

    update

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-$HOME/.local/bin/xcv mocha
-xsetroot -solid "#111222"
+$HOME/.local/bin/xcv manjaroi3
+#xsetroot -solid "#111222"
+[[ ! -z $(which nitrogen 2> /dev/null) ]] && nitrogen --restore
 setxkbmap customo
-brightnessctl 30%
-dunst &
+#brightnessctl 30%
+#dunst &
 [ ! -z $(pgrep -x bspwm) ] && $HOME/.local/bin/bspwm_bar.sh &
 [ ! -z $(pgrep -x dwm) ] && $HOME/.local/bin/dwm_bar.sh &
--- xkb/setup_keyboard_layout.sh
+++ xkb/setup_keyboard_layout.sh
@@ -4,7 +4,7 @@ if [ -d /usr/share/X11/xkb/ ] && [ -f ./customo ] && [ -f evdev.xml ];then
   sudo cp -i ./customo /usr/share/X11/xkb/symbols
   sudo cp -i ./evdev.xml /usr/share/X11/xkb/rules/evdev.xml
   # debian
-  sudo dpkg-reconfigure xkb-data
+  #sudo dpkg-reconfigure xkb-data
   setxkbmap customo
 else
   echo "Failed. Either /usr/share/X11/xkb/ is not a dir or files ./customo ./evdev.xml do not exist"
