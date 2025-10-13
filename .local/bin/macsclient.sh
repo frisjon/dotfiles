9f8d1221d1218b5cd98ba2df735af9e47c086c28
frisjon <frisjon0@gmail.com>
2025-10-12 22:17:22 -0300

    update emacs script. rename bc of bug

--- .local/bin/macsclient.sh
+++ .local/bin/macsclient.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 
+set -euxo pipefail
+
 # this script launches an instance of emacsclient
 # if the emacs server is not running, it executes it
 
-ps cax | grep emacs > /dev/null
-if [ $? -ne 0 ]; then
-    emacs --daemon
-fi
-
-emacsclient -c &
+[[ -z $(ps cax | grep emacs) ]] && emacs --daemon 
+emacsclient -c
